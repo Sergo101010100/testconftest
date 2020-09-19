9c7810f33237db6dc2b89241cfc4262cfcced487
Sergo <TEW6rfHz0>
2020-09-19 13:56:54 +0300

    create test test_button_add_to_basket_form

---
+++ test_items.py
@@ -0,0 +1,10 @@
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_button_add_to_basket_form(browser):
+    browser.get(link)
+   # time.sleep(30)
+    button = browser.find_element_by_xpath("//form[@id='add_to_basket_form']/button")
+    assert  button.is_displayed()
